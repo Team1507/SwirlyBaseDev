3af2d9332e25550934d01568d202b39a492c5e62
K.Sielski <kris@sielski.com>
2023-12-10 11:39:19 -0500

    Basic Drivetrain working

--- src/main/cpp/commands/CmdDriveWithGamepad.cpp
+++ src/main/cpp/commands/CmdDriveWithGamepad.cpp
@@ -4,11 +4,11 @@
 
 #include "Robot.h"
 #include "commands/CmdDriveWithGamepad.h"
+#include <cmath>
 
 CmdDriveWithGamepad::CmdDriveWithGamepad() 
 {
-
-
+  SetName("CmdDriveWithGamepad");
   AddRequirements( &robotcontainer.m_drivetrain );
 }
 
@@ -20,9 +20,37 @@ void CmdDriveWithGamepad::Initialize()
 
 }
 
-
+//Running in Velocity Mode
 void CmdDriveWithGamepad::Execute() 
 {
+
+  //Get Gamepad input;
+  float leftY  = robotcontainer.m_xbox.GetLeftY();
+  float leftX  = robotcontainer.m_xbox.GetLeftX();
+  float rightX = robotcontainer.m_xbox.GetRightX();
+
+  const float xyMaxVelocity = 19600; //
+  const float rMaxVelocity  = 13000; //
+
+  const float xyScaleValue  = 0.5;
+  const float rScaleValue   = 0.3;
+
+
+  //Square Inputs???
+  leftY  = ( leftY  < 0 ) ? -pow(  leftY,  2) : pow(  leftY,  2);
+  leftX  = ( leftX  < 0 ) ? -pow(  leftX,  2) : pow(  leftX,  2);
+  rightX = ( rightX < 0 ) ? -pow( rightX,  2) : pow( rightX,  2);
+
+  //Apply max velocity and scaling
+  float fwdrev    = -(leftY  * xyMaxVelocity * xyScaleValue);    //Invert Axis, make positive forward
+  float rightleft =  (leftX  * xyMaxVelocity * xyScaleValue);    
+  float rotate    = -(rightX * rMaxVelocity  * rScaleValue);     //Invert Axis, make positive CCW
+
+  if( robotcontainer.m_drivetrain.GetDriveType() == Drivetrain::ROBOTCENTRIC )
+    robotcontainer.m_drivetrain.RobotcentricDriveVelocity( fwdrev,  rightleft,  rotate );
+  else
+    robotcontainer.m_drivetrain.FieldcentricDriveVelocity( fwdrev,  rightleft,  rotate );
+
 
 }
 
