f49255eb6138eb7f5d5d1e23cd6cd79b5c5e4bd0
Warlocks1507programmer <programmers@warlocks1507.com>
2024-02-07 17:57:30 -0500

    AutoFourPieceRed/BlueTwo Done

--- src/main/cpp/commands/AutoFourPieceBlueTwo.cpp
+++ src/main/cpp/commands/AutoFourPieceBlueTwo.cpp
@@ -11,5 +11,22 @@
 
 AutoFourPieceBlueTwo::AutoFourPieceBlueTwo() 
 {
-  
+  AddCommands(
+    CmdPrintText("Auto Four Piece Blue Two Started"),
+    CmdDriveClearAll(),
+    //Shoot Note
+    CmdDriveToPoint(0, 40, 0, 2000, true, 10),
+    //Shoot Note
+    CmdDriveToPoint(-50, 0, 0, 2000, true, 10),
+    CmdDriveTurn2Angle(0.2, -45),
+    //Shoot Note
+    CmdDriveTurn2Angle(0.2, 45),
+    CmdDriveToPoint(-19, 0, 0, 2000, true, 10),
+    CmdDriveToPoint(0, -211, 0, 2000, true, 10),
+    CmdDriveToPoint(0, 96, 0, 2000, true, 10),
+    CmdDriveTurn2Angle(0.2, -45),
+    //Shoot Note
+    CmdPrintText("Auto Four Piece Blue Two Ended"),
+    CmdDriveStop()
+  );
 }
